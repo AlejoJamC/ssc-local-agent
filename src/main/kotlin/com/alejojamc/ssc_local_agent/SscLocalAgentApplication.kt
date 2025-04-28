6ba7d30304cda4e22307a662c22099789520ff21
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2025-04-28 20:37:07 +0200

    initial commit, spring initializr with Spring AI and basic modules

---
+++ src/main/kotlin/com/alejojamc/ssc_local_agent/SscLocalAgentApplication.kt
@@ -0,0 +1,11 @@
+package com.alejojamc.ssc_local_agent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SscLocalAgentApplication
+
+fun main(args: Array<String>) {
+	runApplication<SscLocalAgentApplication>(*args)
+}
---
+++ src/test/kotlin/com/alejojamc/ssc_local_agent/SscLocalAgentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alejojamc.ssc_local_agent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SscLocalAgentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
